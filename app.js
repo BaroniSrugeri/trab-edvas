4c077de4c3c94ccf69215e0bee1039c5452a623b
Baroni Rugeri <162318601+BaroniSrugeri@users.noreply.github.com>
2025-03-19 14:55:39 -0300

    app.js

--- app.js
+++ app.js
@@ -25,3 +25,66 @@ document.getElementById('adicionar-prod').addEventListener('click', function () 
 });
 
 
+
+
+
+
+document.getElementById('adicionar').addEventListener('click',
+    function () {
+
+        const itemSelecionado = document.getElementById('item').value;
+
+        if (itemSelecionado) {
+            const listalivros1 = document.getElementById('lista-livros1');
+            const novoItem = document.createElement('ul1');
+            novoItem.textContent = itemSelecionado;
+            listalivros1.appendChild(novoItem);
+        }
+    });
+
+
+document.getElementById('adicionar-prod1').addEventListener('click', function () {
+    var novoTexto = document.getElementById('texto1').value;
+    if (novoTexto) {
+        var select = document.getElementById('item1');
+        var novaOpcao = document.createElement('option');
+        novaOpcao.text = novoTexto;
+        novaOpcao.value = novoTexto.toLowerCase().replace(/\s+/g, '-');
+        select.appendChild(novaOpcao);
+        document.getElementById('texto').value = ''; 
+    }
+});
+
+
+
+
+
+
+
+document.getElementById('adicionar').addEventListener('click',
+    function () {
+
+        const itemSelecionado = document.getElementById('item2').value;
+
+        if (itemSelecionado) {
+            const listalivros2 = document.getElementById('lista-livros2');
+            const novoItem = document.createElement('ul2');
+            novoItem.textContent = itemSelecionado;
+            listalivros2.appendChild(novoItem);
+        }
+    });
+
+
+document.getElementById('adicionar-prod2').addEventListener('click', function () {
+    var novoTexto = document.getElementById('texto2').value;
+    if (novoTexto) {
+        var select = document.getElementById('item2');
+        var novaOpcao = document.createElement('option');
+        novaOpcao.text = novoTexto;
+        novaOpcao.value = novoTexto.toLowerCase().replace(/\s+/g, '-');
+        select.appendChild(novaOpcao);
+        document.getElementById('texto2').value = ''; 
+    }
+});
+
+
